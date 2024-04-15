f5ad8630732d93021d79d973d830be30e2dd8735
zongzw <zongzw@zongzw-MacBook-Pro.local>
2024-04-15 10:46:57 +0800

    add softplus activation func.

--- scripts/plot-activation-functions.py
+++ scripts/plot-activation-functions.py
@@ -15,10 +15,15 @@ axes[0, 1].set_title('tanh')
 
 x = np.arange(-5, 0, 0.05)
 y = np.zeros(len(x))
-axes[1, 0].plot(x, y)
+axes[1, 0].plot(x, y, c='b')
 x = np.arange(0, 5, 0.05)
 y = x
 axes[1, 0].plot(x, y)
 axes[1, 0].set_title('ReLu')
+
+x = np.arange(-5, 5, 0.05)
+y = np.log(1+np.exp(x))
+axes[1, 1].plot(x, y)
+axes[1, 1].set_title('softplus')
 
 plt.show()
