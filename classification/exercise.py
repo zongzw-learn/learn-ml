780fe14a49e4fb58429e03a80d75d2cfcce22dea
zongzw <zongzhaowei-2002@163.com>
2024-07-31 18:11:50 +0800

    add make_moons exercise.

--- classification/exercise.py
+++ classification/exercise.py
@@ -2,13 +2,77 @@ from sklearn.datasets import make_moons
 from sklearn.model_selection import train_test_split
 
 import matplotlib.pyplot as plt
+import torch
+from torch import nn
+from models import CircleModelV2
+from helper_functions import plot_decision_boundary, accuracy_fn
 
 n_sample=1000
 
-X, y = make_moons(n_samples=n_sample, noise=0.03, random_state=42)
+X, y = make_moons(n_samples=n_sample, noise=0.2, random_state=42)
 
 plt.figure(figsize=(10, 7))
 plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.RdYlBu)
+# plt.show()
+
+
+X = torch.from_numpy(X).type(torch.float)
+y = torch.from_numpy(y).type(torch.float)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(device)
+
+model_3 = CircleModelV2().to(device=device)
+print(model_3)
+
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.SGD(model_3.parameters(), lr=0.1)
+
+torch.manual_seed(42)
+
+epochs = 6000
+X_train, y_train = X_train.to(device), y_train.to(device)
+X_test, y_test = X_test.to(device), y_test.to(device)
+
+for epoch in range(epochs):
+    y_logits = model_3(X_train).squeeze()
+    y_pred = torch.round(torch.sigmoid(y_logits))
+
+    loss = loss_fn(y_logits, y_train)
+    acc = accuracy_fn(y_true=y_train, y_pred=y_pred)
+
+    optimizer.zero_grad()
+
+    loss.backward()
+
+    optimizer.step()
+
+    model_3.eval()
+    with torch.inference_mode():
+        test_logits = model_3(X_test).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))
+
+        test_loss = loss_fn(test_logits, y_test)
+        test_acc = accuracy_fn(y_true=y_test, y_pred=test_pred)
+
+        if epoch % 100 == 0:
+            print(f"Epoch: {epoch} | "
+                  f"Loss: {loss: .5f}, Accuracy: {acc: .5f}% | "
+                  f"Test Loss: {test_loss: .2f}, Test Accuracy: {test_acc: .2f}%")
+
+
+from helper_functions import plot_predictions, plot_decision_boundary
+import matplotlib.pyplot as plt
+# Plot decision boundaries for training and test sets
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Train")
+
+plot_decision_boundary(model_3, X_train, y_train) # model_1 = no non-linearity
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model_3, X_test, y_test) # model_3 = has non-linearity
+
 plt.show()
-
-
